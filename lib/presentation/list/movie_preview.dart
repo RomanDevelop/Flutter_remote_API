a91f5af9a6a196520955e54d1c09ef17b4bf7cbb
roman.k <roman.k@3sda-evayadatasystems.com>
2023-11-27 11:46:17 +0100

    Added detail screen

--- lib/presentation/list/movie_preview.dart
+++ lib/presentation/list/movie_preview.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/domain/model/movie.dart';
+import 'package:flutter_application_2/presentation/detail/movie_detail_screen.dart';
 import 'package:intl/intl.dart';
 
 class MoviePreview extends StatelessWidget {
@@ -14,70 +15,77 @@ class MoviePreview extends StatelessWidget {
   Widget build(BuildContext context) {
     final DateFormat formatter = DateFormat.yMMMd();
 
-    return Card(
-      child: Row(
-        children: [
-          // Movie image
-          if (movie.imageUrl != null)
-            Container(
-              padding: const EdgeInsets.only(
-                left: 8.0,
-                top: 8.0,
-                right: 16.0,
-                bottom: 8.0,
+    return GestureDetector(
+      onTap: () => Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => MovieDetailScreen(movie: movie),
+        ),
+      ),
+      child: Card(
+        child: Row(
+          children: [
+            // Movie image
+            if (movie.imageUrl != null)
+              Container(
+                padding: const EdgeInsets.only(
+                  left: 8.0,
+                  top: 8.0,
+                  right: 16.0,
+                  bottom: 8.0,
+                ),
+                child: SizedBox(
+                  width: _size,
+                  height: _size,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(5.0),
+                    child: CachedNetworkImage(
+                      imageUrl: movie.imageUrl!,
+                    ),
+                  ),
+                ),
+              )
+            else
+              Container(
+                padding: const EdgeInsets.only(
+                  left: 8.0,
+                  top: 8.0,
+                  right: 16.0,
+                  bottom: 8.0,
+                ),
+                child: const _PlaceholderImage(size: _size),
               ),
-              child: SizedBox(
-                width: _size,
-                height: _size,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(5.0),
-                  child: CachedNetworkImage(
-                    imageUrl: movie.imageUrl!,
+            // Movie title and release date
+            Flexible(
+              child: Container(
+                padding: const EdgeInsets.only(
+                  left: 16.0,
+                  top: 12.0,
+                  right: 16.0,
+                  bottom: 12.0,
+                ),
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        movie.title,
+                        style: Theme.of(context).textTheme.titleLarge,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 12.0),
+                        child: Text(
+                          formatter.format(movie.releaseDate),
+                          style: Theme.of(context).textTheme.bodySmall,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
-            )
-          else
-            Container(
-              padding: const EdgeInsets.only(
-                left: 8.0,
-                top: 8.0,
-                right: 16.0,
-                bottom: 8.0,
-              ),
-              child: const _PlaceholderImage(size: _size),
             ),
-          // Movie title and release date
-          Flexible(
-            child: Container(
-              padding: const EdgeInsets.only(
-                left: 16.0,
-                top: 12.0,
-                right: 16.0,
-                bottom: 12.0,
-              ),
-              child: Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      movie.title,
-                      style: Theme.of(context).textTheme.titleLarge,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 12.0),
-                      child: Text(
-                        formatter.format(movie.releaseDate),
-                        style: Theme.of(context).textTheme.bodySmall,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
